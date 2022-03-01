d104ad06e3028e98d0e41ab2c586e7cbf3f56d9e
Vinycius <viny.antunes@gmail.com>
2022-03-01 15:53:26 -0300

    começando o rastreio

---
+++ centuryFromYear.js
@@ -0,0 +1,9 @@
+function solution(year) {
+    let centuryCount = 0;
+    while (year > 0){
+    year = year - 100;
+    centuryCount = centuryCount + 1;
+    }
+    return centuryCount;
+}
+console.log(solution(year));
