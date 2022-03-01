89a435e3444c19c6e86a5c12b03787764e5749ff
Vinycius <viny.antunes@gmail.com>
2022-03-01 15:58:30 -0300

    add.js

---
+++ add.js
@@ -0,0 +1,6 @@
+function solution(param1, param2) {
+    return param1 + param2;
+}
+                    //substitu o param1,2 por numeros.
+                     //10    //5    //pode usar estes como exemplo
+console.log(solution(param1, param2));
